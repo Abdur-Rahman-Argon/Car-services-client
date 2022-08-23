96546863409b8a31ff7aa25b503686c03f41aafb
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-23 19:55:48 +0600

    page title add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Pages/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1> Car Service point</h1>
+      {/*  */}
+
+      <Home></Home>
+
+      {/*  */}
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageTitle from "../../Shared/PageTitle/PageTitle";
+
+const Home = () => {
+  return (
+    <>
+      <PageTitle title="Home"></PageTitle>
+    </>
+  );
+};
+
+export default Home;
