727c23437b794a980969fca502f063178ebaa163
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-08-23 20:07:54 +0600

    bannar added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PageTitle from "../../Shared/PageTitle/PageTitle";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <>
       <PageTitle title="Home"></PageTitle>
+      <Banner></Banner>
     </>
   );
 };
